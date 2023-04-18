619b252effa8d75eec9624c8ba1552e35a0efe54
OndrejKnebl <72308047+OndrejKnebl@users.noreply.github.com>
2023-04-18 17:00:59 +0200

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,6 @@
+export interface SimplePanelOptions {
+    targetTTNServer: string;            // URL of the ttn server (network cluster) is stored here
+    targetAPIkey: string;               // API key from TTS is stored here
+    targetAppName: string;              // Application name is stored here
+    targetEndDeviceName: string;        // End device name is stored here
+}
